b6a774b218dc8bda84b8f060a722d0c89b126b0a
Mahmoud taha <mahmoud.taha@idemia.com>
2023-08-27 12:44:27 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import LayoutWrapper from "./UI/LayoutWrapper/LayoutWrapper";
+import Home from "./Views/Home/Home";
+import CurrencyDetails from "./Views/CurrencyDetails/CurrencyDetails";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LayoutWrapper />,
+      children: [
+        {
+          path: '/',
+          loader: () => redirect("/home")
+        },
+        {
+          path: '/home',
+          element: <Home />
+        },
+
+        {
+          path: '/details',
+          element: <CurrencyDetails />,
+        },
+      ],
+    },
+    {
+      path: "*",
+      loader: () => redirect("/home")
+    },
+  ]);
   return (
-    <div className="App">
-      
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/UI/LayoutWrapper/LayoutWrapper.js
@@ -0,0 +1,6 @@
+const LayoutWrapper = () => {
+    return (
+        <div> LayoutWrapper Component Working </div>
+    )
+}
+export default LayoutWrapper;
