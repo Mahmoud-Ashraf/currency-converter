cb00890ac86b6c595b49573af643052c54b83f04
Mahmoud taha <mahmoud.taha@idemia.com>
2023-08-28 17:33:59 +0300

    add UI to 3*3 cards on home page and implement 9 deffirent currencies

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -1,6 +1,39 @@
+import ConvertedCard from "../../components/ConvertedCard/ConvertedCard";
+
 const Home = () => {
     return (
-        <div> Home Component Working </div>
+
+        <div className="container-fluid mt-5">
+            <div className="row g-3">
+                <div className="col-4">
+                    <ConvertedCard to="EGP" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="AED" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="SAR" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="USD" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="QAR" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="INR" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="JPY" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="KWD" />
+                </div>
+                <div className="col-4">
+                    <ConvertedCard to="CNY" />
+                </div>
+            </div>
+        </div>
     )
 }
 export default Home;
--- src/components/ConvertedCard/ConvertedCard.js
+++ src/components/ConvertedCard/ConvertedCard.js
@@ -1,6 +1,34 @@
-const ConvertedCard = () => {
+import { useSelector } from "react-redux";
+import useHTTP from "../../Hooks/use-http";
+import { useEffect, useState } from "react";
+
+const ConvertedCard = (props) => {
+    const [rate, setRate] = useState(0);
+    const convertion = useSelector(state => state.convertion);
+    const { sendRequest } = useHTTP();
+
+    useEffect(() => {
+        convertCurrency();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [convertion.from, props.to]);
+
+    const convertCurrency = () => {
+        sendRequest(
+            {
+                url: `latest?base=${convertion.from}&symbols=${props.to}`,
+                method: 'GET'
+            },
+            data => {
+                setRate(data.rates[props.to]);
+            },
+            err => { }
+        )
+    }
     return (
-        <div> ConvertedCard Component Working </div>
+        <div className="border rounded p-5 text-center">
+            <p className="fs-5 mb-0">1.00 {convertion.from} = {rate || 'XX.XXX'} {props.to}</p>
+            <p className="fs-5 mb-0">{convertion.amount} {convertion.from} = {(rate * convertion.amount) || 'XX.XX'} {props.to}</p>
+        </div>
     )
 }
 export default ConvertedCard;
