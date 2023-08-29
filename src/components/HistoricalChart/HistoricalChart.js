372237fa2afd443747acc2336d4f2e98816b80ef
Mahmoud taha <mahmoud.taha@idemia.com>
2023-08-30 00:13:55 +0300

    add comments for historical chart component

--- src/components/HistoricalChart/HistoricalChart.js
+++ src/components/HistoricalChart/HistoricalChart.js
@@ -30,6 +30,7 @@ const HistoricalChart = () => {
     const [chartData, setChartData] = useState([]);
     const [month, setMonth] = useState(new Date().getMonth());
     const [year, setYear] = useState(new Date().getFullYear());
+    // set labels and sort them acoording to current month which months on current year and which from previous year.
     const [labels, setLabels] = useState([...Months.filter((_, i) => i >= month), ...Months.filter((_, i) => i < month)]);
     const convertion = useSelector(state => state.convertion);
     const options = {
@@ -51,6 +52,7 @@ const HistoricalChart = () => {
     }, []);
 
     useEffect(() => {
+        // if all months data appended to chart data then set this data to chart data.
         if (chartData.length === 12) {
             setData(
                 {
@@ -70,12 +72,14 @@ const HistoricalChart = () => {
     }, [chartData]);
 
     useEffect(() => {
+        // set chart data and rerender chart for every change on base currency or target currency.
         setChartData([]);
         renderChart();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [convertion.from, convertion.to])
 
     const getChartData = (date) => {
+        // send request to get data for last day in each month and append this data to chart data.
         sendRequest(
             {
                 url: `${date}?&base=${convertion.from}&symbols=${convertion.to}`,
@@ -90,7 +94,9 @@ const HistoricalChart = () => {
 
     const renderChart = () => {
         // work around because fixer.io historical API needs supscription.
+        // i Used historical API with some work around to get the rate of last day of each month for the last year.
         for (let index = 1; index <= 12; index++) {
+            // loop through months to get last day in each month and generate the accurate date to get the data.
             index = index < 10 ? ('0' + index) : index;
             if (Number(index) > month) {
                 const lastDayOfMonth = new Date(year - 1, index, 0).getDate();
